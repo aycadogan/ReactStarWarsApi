6c385983180a724e30cfa75c265ec6cec0b1d936
aycadogan89 <doganayca89@gmail.com>
2021-11-18 22:21:28 -0800

    router

--- starwarsapi/src/components/People.jsx
+++ starwarsapi/src/components/People.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import '../App.css'
-import SearchCharacter from './SearchCharacter.jsx'
 
 const People = () => {
 
@@ -16,7 +15,6 @@ const People = () => {
     return (
        
         <div className="wrapper">
-            <SearchCharacter items={items}/>
             <ul className="card-grid">
             {items && items.map((item) => (
                 <li>
